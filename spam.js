165da804e303797fd07a8321235f2e60683ea66f
miwanoff <marina.iva.noffa@gmail.com>
2025-04-13 10:36:08 +0300

    Add checkSpam

---
+++ spam.js
@@ -0,0 +1,13 @@
+function checkSpam(str) {
+  // let str = text;
+  const spam = "spam";
+  if (/* знайшли слово spam ? */) {
+ 
+  }
+  /* інакше */
+
+}
+
+const text = prompt("Input a string");
+
+checkSpam(text);
